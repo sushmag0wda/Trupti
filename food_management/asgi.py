4a12cbd9ad1670661ce3ed33b0ae8a75ed72a79e
Sushma Gowda <sushgowda808@gmail.com>
2025-11-03 00:04:39 +0530

    Delete food_management directory

--- food_management/asgi.py
+++
@@ -1,8 +0,0 @@
-"""ASGI config for food_management project."""
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "food_management.settings")
-
-application = get_asgi_application()
--- food_management/wsgi.py
+++
@@ -1,8 +0,0 @@
-"""WSGI config for food_management project."""
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "food_management.settings")
-
-application = get_wsgi_application()
